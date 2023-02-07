5a7153b9cab346075ae325327699c8266b47a582
Damian Wieszczak <97399192+DamianWieszczak@users.noreply.github.com>
2023-02-07 18:50:38 +0100

    Revert "Adding Employee feature with devise gem, add sign up, sign in and sign out options for employee"

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root "home#index"
-  get "home/index", to: "home#index"
-  devise_for :employees
-  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
