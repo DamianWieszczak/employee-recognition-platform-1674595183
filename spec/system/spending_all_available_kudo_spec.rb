cee98e453a3d38e41762abf3bea535190e243aa0
DamianWieszczak <damian.wieszczak@gmail.com>
2023-03-07 00:14:29 +0100

    Add number of availabvle kudos, add default value of kudos, create system spec

---
+++ spec/system/spending_all_available_kudo_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'No available kudos', type: :system do
+  let(:employee) { create :employee }
+
+  context 'when user has no kudos here' do
+    before do
+      login_as(employee, scope: :employee)
+      employee.number_of_available_kudos = 0
+    end
+
+    it 'can not create kudo via button and url visit' do
+      visit kudos_path
+      expect(page).not_to have_button('New Kudo')
+      visit 'kudos/new'
+      expect(page).to have_content('You have no more kudos to give')
+    end
+  end
+end
