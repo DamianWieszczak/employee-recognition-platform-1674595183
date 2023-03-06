a48ff7a8582f3ecabc58cb8a9a2307bd0b5a877c
DamianWieszczak <damian.wieszczak@gmail.com>
2023-03-06 21:05:20 +0100

    Add some minor changes to improve to improve the quality of the task

--- app/controllers/admins/kudos_controller.rb
+++ app/controllers/admins/kudos_controller.rb
@@ -1,24 +1,17 @@
 module Admins
   class KudosController < AdminsController
-    before_action :set_kudo, only: [:destroy]
-
     def index
       @kudos = Kudo.all.order(created_at: :desc)
     end
 
     def destroy
+      @kudo = Kudo.find(params[:id])
       if @kudo.destroy
         flash[:notice] = 'Kudo was successfully deleted'
       else
         flash[:alert] = 'Delete Kudo failed'
       end
       redirect_to admins_kudos_path
-    end
-
-    private
-
-    def set_kudo
-      @kudo = Kudo.find(params[:id])
     end
   end
 end
--- spec/system/admin_kudos_delete_spec.rb
+++ spec/system/admin_kudos_delete_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 RSpec.describe '#delete?', type: :system do
   let!(:admin) { create :admin }
-  let!(:employee) { create :employee }
-  let!(:kudo) { create :kudo, giver: employee }
+  let!(:kudo) { create :kudo }
 
   it 'Deleting kudos by admin' do
     login_as(admin, scope: :admin)
