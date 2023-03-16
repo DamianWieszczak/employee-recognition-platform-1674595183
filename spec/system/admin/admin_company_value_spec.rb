6bc0a0b8be19c0ee6a55347e0bf6e688a03f6a86
DamianWieszczak <damian.wieszczak@gmail.com>
2023-03-16 01:13:22 +0100

    Add fixes for rspec

--- spec/system/admin/admin_company_value_spec.rb
+++ spec/system/admin/admin_company_value_spec.rb
@@ -6,32 +6,32 @@ RSpec.describe 'Company Value CRUD actions', type: :system do
 
   context 'when using CRUD actions' do
     before do
-      login_as(:admin)
+      login_as(admin, scope: :admin)
     end
 
     it 'Check listed Company Value' do
       visit admins_company_values_path
-      expect(page).to have_text company_value.title
+      expect(page).to have_content(company_value.title)
     end
 
     it 'Creates Company Values' do
       visit admins_company_values_path
       click_link 'New Company Value'
       fill_in 'Title', with: Faker::Company.industry
-      click_link 'Create Company value'
-      expect(page).to have_conent 'Company Value was successfully saved'
+      click_button 'Create'
+      expect(page).to have_content 'Company Value was successfully saved'
     end
 
     it 'Check edit function' do
       visit admins_company_values_path
       click_link 'Edit'
       fill_in 'Title', with: 'Edited Title'
-      click_link 'Update Company value'
+      click_button 'Update'
       expect(page).to have_content 'Company Value was updated'
     end
 
     it 'deletes company value' do
-      visit admnins_company_values_path
+      visit admins_company_values_path
       click_link 'Delete'
       expect(page).to have_content 'Company Value was successfully deleted'
     end
