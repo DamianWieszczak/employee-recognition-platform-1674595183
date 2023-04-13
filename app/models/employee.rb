509e1630e824ffa7ce661fd38e330e691976e711
DamianWieszczak <damian.wieszczak@gmail.com>
2023-04-13 21:39:07 +0200

    Create Order model and controller, employee can order rewards, add routes and migration

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -6,6 +6,7 @@ class Employee < ApplicationRecord
 
   has_many :received_kudos, class_name: 'Kudo', foreign_key: 'receiver_id', dependent: :destroy, inverse_of: :receiver
   has_many :given_kudos, class_name: 'Kudo', foreign_key: 'giver_id', dependent: :destroy, inverse_of: :giver
+  has_many :orders, dependent: :destroy, inverse_of: :employee
 
   private
 
