e8440d4a2d246757c8483464c0aace5c92657af4
DamianWieszczak <damian.wieszczak@gmail.com>
2023-04-24 22:16:50 +0200

    Adding the possibility of using rewards for an employee

--- app/controllers/employees/orders_controller.rb
+++ app/controllers/employees/orders_controller.rb
@@ -1,15 +1,19 @@
 module Employees
   class OrdersController < EmployeesController
+    def index
+      render :index, locals: { orders: Order.all }
+    end
+    
     def create
-      @reward = Reward.find(params[:reward])
-      if current_employee.number_of_earned_points < @reward.price
+      reward = Reward.find(params[:reward])
+      if current_employee.number_of_earned_points < reward.price
         flash[:alert] = 'You dont have enough points to buy a reward'
         redirect_to employees_rewards_path
       else
         ActiveRecord::Base.transaction do
-          @order = Order.new(employee: current_employee, reward: @reward)
-          @order.save!
-          current_employee.update!(number_of_earned_points: current_employee.number_of_earned_points - @reward.price)
+          order = Order.new(employee: current_employee, reward: reward)
+          order.save!
+          current_employee.update!(number_of_earned_points: current_employee.number_of_earned_points - reward.price)
           flash[:notice] = 'You have successfully purchased a reward'
           redirect_to employees_rewards_path
         rescue ActiveRecord::RecordInvalid
