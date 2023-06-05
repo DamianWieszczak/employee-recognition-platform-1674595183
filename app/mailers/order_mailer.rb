c4dcea375eeb930237243c0d1b45683f3ba11f57
DamianWieszczak <damian.wieszczak@gmail.com>
2023-06-05 21:09:43 +0200

    Add fixes for rubocop, rspec

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,6 +1,6 @@
 class OrderMailer < ApplicationMailer
   def confirmation_mail
     @order = params[:order]
-    mail(to: @order.employee.email, subject: "Your order has been successfully delivered")
+    mail(to: @order.employee.email, subject: 'Your order has been successfully delivered')
   end
 end
