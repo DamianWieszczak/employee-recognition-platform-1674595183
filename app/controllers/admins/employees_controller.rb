b5a8207dd77ed4510bd35c24e4cbd8c57e2c732f
DamianWieszczak <damian.wieszczak@gmail.com>
2023-06-13 22:39:45 +0200

    Add fix for rubocop

--- app/controllers/admins/employees_controller.rb
+++ app/controllers/admins/employees_controller.rb
@@ -37,7 +37,7 @@ module Admins
           flash[:alert] = "It's possible to only add kudos from 1 to 20 for each employee"
           render 'add_kudos_to_all'
         end
-      else 
+      else
         flash[:alert] = "It's possible to only add kudos from 1 to 20 for each employee"
         render 'add_kudos_to_all'
       end
