066f07aee471f8d63a2562fc0dc686eb8160f43d
DamianWieszczak <damian.wieszczak@gmail.com>
2023-06-13 22:06:19 +0200

    Add function for admin to add kudos for all employees

--- app/controllers/admins/employees_controller.rb
+++ app/controllers/admins/employees_controller.rb
@@ -18,6 +18,28 @@ module Admins
       end
     end
 
+    def edit_kudos_to_all
+      @employees = Employee.all
+      render 'admins/employees/add_kudos_to_all'
+    end
+
+    def add_kudos_to_all
+      new_amount_of_kudos = amount_params
+      if new_amount_of_kudos < 21
+        Employee.transaction do
+          Employee.find_each do |employee|
+            employee.update!(number_of_available_kudos: employee.number_of_available_kudos + new_amount_of_kudos)
+          end
+        flash[:notice] = "Successfully added the specified amount of kudos for each employee"
+        redirect_to admins_dashboard_path
+
+        rescue ActiveRecord::RecordInvalid
+          flash[:alert] = "It's possible to only add kudos from 1 to 20 for each employee"
+          render 'add_kudos_to_all'
+        end
+      end
+    end
+
     def destroy
       @employee = Employee.find(params[:id])
       if @employee.destroy
@@ -32,6 +54,10 @@ module Admins
 
     def employee_params
       params.require(:employee).permit(:email, :password, :number_of_available_kudos)
+    end
+
+    def amount_params
+      params[:amount].to_i
     end
   end
 end
