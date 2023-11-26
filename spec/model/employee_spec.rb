b6d34bee52f0cf9bb96a2752c296a02be61a253b
DamianWieszczak <damian.wieszczak@gmail.com>
2023-11-26 23:47:40 +0100

    Add fixes in registration pages. Fixes have been made for rubocop and rspec

--- spec/model/employee_spec.rb
+++ spec/model/employee_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-Rspec.describe Employee, type: :model do
+RSpec.describe Employee, type: :model do
   before do
     create(:employee)
   end
 
   it { is_expected.to validate_presence_of(:first_name) }
-  it { is_expected.to validate_presence_of(:last_name)}
+  it { is_expected.to validate_presence_of(:last_name) }
 end
--- spec/system/employee/employee_sign_up_spec.rb
+++ spec/system/employee/employee_sign_up_spec.rb
@@ -4,11 +4,12 @@ RSpec.describe 'Sign up process', type: :system do
   it 'enables me to sign up an employee' do
     visit root_path
     click_link 'Sign Up'
-    fill_in 'Email', with: employee.email
-    fill_in 'First name', with: employee.first_name
-    fill_in 'Last name', with: employee.last_name
-    fill_in 'Password', with: employee.password
+    fill_in 'Email', with: 'example@email.com'
+    fill_in 'First name', with: 'Name'
+    fill_in 'Last name', with: 'Surname'
+    fill_in 'Password', with: 'password'
+    fill_in 'employee_password_confirmation', with: 'password'
     click_button 'Sign up'
-    expect(page).to have_content 'Signed up successfully.'
+    expect(page).to have_content 'Welcome! You have signed up successfully. '
   end
 end
